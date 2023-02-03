ca623699a0ed332fe326bd5e4b13764827a28a2e
Steve <steve@tryshophub.com>
2023-02-03 21:34:47 +0000

    Create 4x4 grid using css grid

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const gridContainer = document.querySelector('#grid-container');
+
+//Creates grid item
+
+
+
+for (i = 0; i < 16; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+    };
+
+
+
+
+
